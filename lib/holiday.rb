b298703126d24ee8375c0060c660d0ce488d9848
Stacy Carr <stacy@boostersinabox.com>
2018-07-17 15:10:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
       puts "#{holiday.to_s.split(' ').collect {|second_word| second_word.capitalize}.join(' ')}: #{supplies}.join(, )}"
-    
-
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
